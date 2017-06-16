a50a26effa69fa80223d980d13f7a7605f5b2513
TrikTron <victor.danel998@gmail.com>
2017-06-16 14:32:09 +0200

    mise en comment lien bdd
    pas de lien ave la bdd régler le probleme soon
    Voir Mr.Vion ?

--- Projet_S2/src/Main.java
+++ Projet_S2/src/Main.java
@@ -1,12 +1,13 @@
-import java.sql.Connection;
+//import java.sql.Connection;
 
 public class Main {
 
 	public static void main(String[] args) {
-		PGSimpleDataSource ds= new PGSimpleDataSource();
+	/*	PGSimpleDataSource ds= new PGSimpleDataSource();
 		ds.setSeverName("postgres");
 		ds.setDataBaseName("Projet_S2");
 		Connection con = ds.getConnection ("postgres","admin");
+		*/
 		Accueil Accueil =new Accueil();
 	}
 
